5e27571ee4cc93c0bde186a17cdbee3d4e8da81c
tenecort <karapostolis.ioannis@gmail.com>
2020-02-21 14:08:56 +0100

    Yo, adding the arduino script in the repo

---
+++ aSketch/aSketch.ino
@@ -0,0 +1,75 @@
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x12
+
+int dataReceived = 0;
+int modules[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+int activeModules[] = {};
+int activeModuleIndex = 0;
+
+void setup() {
+    Serial.begin(9600);
+    Wire.begin(SLAVE_ADDRESS);
+    Wire.onReceive(receiveData);
+    Wire.onRequest(sendData);
+}
+
+void loop() {
+    delay(100);
+}
+
+void receiveData(int byteCount)
+{
+    while(Wire.available())
+    {
+        dataReceived = Wire.read();
+        
+        if(dataReceived < 100)
+        {
+          Serial.print("Durée du script : ");
+          Serial.println(dataReceived);
+        }
+        
+        if(dataReceived > 100 && dataReceived < 200)
+        {
+          activeModules[activeModuleIndex] = dataReceived - 100;
+          activeModuleIndex++;
+          Serial.print("Module actif : ");
+          Serial.println(dataReceived - 100);
+        }
+        
+        if(dataReceived == 200)
+        {
+          Serial.print("I listen ");
+          Serial.println(activeModuleIndex);
+        }
+    }
+}
+
+void sendData()
+{
+    int envoi = dataReceived;
+    if(dataReceived < 100)
+    {
+      Wire.write(envoi);
+    }
+    
+    if(dataReceived > 100 && dataReceived < 200)
+    {
+      Wire.write(envoi);
+    }
+    
+    if(dataReceived == 200)
+    {
+      Serial.print("Modules remaining ");
+      Serial.println(activeModuleIndex);
+      Wire.write(activeModuleIndex);
+      activeModuleIndex--;
+    }
+    
+}
+
+void checkModules()
+{
+  
+}
